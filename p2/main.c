9f0bc78b9455c3feb339f428811aba6dee8f0532
2025050156 <gecchopper273@gmail.com>
2026-04-20 15:14:20 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+	int ecnt = 0, ncnt = 0;
+	int maxe = 0, maxn = 0;
+
+	int length = 0;
+	int i = 0;
+
+	char c = '\0';
+
+	scanf("%d", &length);
+	getchar();  /* 기존 강의자료대로. */
+	for(i=0; i<length; i++) {
+		scanf("%c", &c);  /* getchar를 안 다뤘으므로... */
+		if(c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
+			ecnt++;
+			if(ncnt > 0) {
+				if(maxn < ncnt) maxn = ncnt;
+				ncnt = 0;
+			}
+		} else if(c >= '0' && c <= '9') {
+			ncnt++;
+			if(ecnt > 0) {
+				if(maxe < ecnt) maxe = ecnt;
+				ecnt = 0;
+			}
+		}
+	}
+
+	printf("%d\n%d", maxe, maxn);
+
+	return 0;
+}
