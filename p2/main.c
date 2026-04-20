01c955b26b2636c4f8103df3c52336ccb5361ce2
2025050156 <gecchopper273@gmail.com>
2026-04-20 15:29:40 +0900

    검사 후 수정

--- p2/main.c
+++ p2/main.c
@@ -11,25 +11,20 @@ int main(void) {
 
 	scanf("%d", &length);
 	getchar();  /* 기존 강의자료대로. */
-	for(i=0; i<=length; i++) {  /* 마지막에 개행을 받기 위해 한 문자 더 받음. */
+	for(i=0; i<length; i++) {  /* 마지막에 개행을 받기 위해 한 문자 더 받음. */
 		scanf("%c", &c);  /* getchar를 안 다뤘으므로... */
-		if(c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
+		if(c >= 'a' && c <= 'z') {
 			ecnt++;
-			if(ncnt > 0) {
-				if(maxn < ncnt) maxn = ncnt;
-				ncnt = 0;
-			}
+			if(maxn < ncnt) maxn = ncnt;
+			ncnt = 0;
 		} else if(c >= '0' && c <= '9') {
 			ncnt++;
-			if(ecnt > 0) {
-				if(maxe < ecnt) maxe = ecnt;
-				ecnt = 0;
-			}
-		} else {
-			if(maxn < ncnt) maxn = ncnt;
 			if(maxe < ecnt) maxe = ecnt;
+			ecnt = 0;
 		}
 	}
+	if(maxn < ncnt) maxn = ncnt;
+	if(maxe < ecnt) maxe = ecnt;
 
 	printf("%d\n%d", maxe, maxn);
 
